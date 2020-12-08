179b6b160dfa2a105652a1ac84a7ea2d9a40d613
Tran Khanh <tran_khanh@outlook.com>
2020-12-08 17:28:56 +0700

    update

--- Scripts/autopep8-script.py
+++ Scripts/autopep8-script.py
@@ -1,6 +1,6 @@
 #!c:\users\khanh.desktop-aq0hcuh\source\repos\python-horse\scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'install-requires==0.3.0','console_scripts','install-requires'
-__requires__ = 'install-requires==0.3.0'
+# EASY-INSTALL-ENTRY-SCRIPT: 'autopep8==1.5.4','console_scripts','autopep8'
+__requires__ = 'autopep8==1.5.4'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('install-requires==0.3.0', 'console_scripts', 'install-requires')()
+        load_entry_point('autopep8==1.5.4', 'console_scripts', 'autopep8')()
     )
